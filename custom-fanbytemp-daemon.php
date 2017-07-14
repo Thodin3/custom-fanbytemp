2cd8899057f6c3eb8d860644926a08c929c4f21b
Thodin3 <mistermystere3@yahoo.fr>
2017-07-15 00:15:00 +0200

    Licence Headers

--- custom-fanbytemp-daemon.php
+++ custom-fanbytemp-daemon.php
@@ -1,4 +1,19 @@
 <?php
+/*
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version. See LICENSE for more details.
+	 
+ 	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See LICENSE for more details.
+	
+	The author can't BE LIABLE TO YOU FOR ANY DAMAGE, INCLUDING ANY
+	GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE
+	USE OR INABILITY TO USE THE PROGRAM. See LICENSE for more details.
+ */
 
 // Init constants
 const FAN_COMMAND = 'fan';
